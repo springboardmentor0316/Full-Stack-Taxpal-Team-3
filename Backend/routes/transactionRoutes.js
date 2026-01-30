974c14303eae907977387fab3d2ab5f2c7a3ede3
Manasvi0904 <adusumallimanasvi@gmail.com>
2026-01-30 13:14:15 +0530

    Update transactionRoutes.js

--- Backend/routes/transactionRoutes.js
+++ Backend/routes/transactionRoutes.js
@@ -1,3 +1,5 @@
+console.log("✅ transactionRoutes.js LOADED");
+
 const express = require("express");
 const verifyToken = require("../middlewares/authMiddleware");
 const {
@@ -6,6 +8,8 @@ const {
   getIncome,
   getExpenses,
   getAllTransactions,
+   updateTransaction,
+  deleteTransaction,
 } = require("../controllers/transactionController");
 
 const router = express.Router();
@@ -18,6 +22,15 @@ router.get("/income", verifyToken, getIncome);
 router.post("/expense", verifyToken, addExpense);
 router.get("/expense", verifyToken, getExpenses);
 // ALL TRANSACTIONS (income + expense)
+// ALL TRANSACTIONS
 router.get("/", verifyToken, getAllTransactions);
+
+// UPDATE
+router.put("/:id", verifyToken, updateTransaction);
+
+// DELETE
+router.delete("/:id", verifyToken, deleteTransaction);
+
+
 
 module.exports = router;
